0ddf7211119c45d532c6060f3df2fe155aaa8e5b
Motjiang <motjiangbrilliant@gmail.com>
2024-11-17 14:29:15 +0200

    Added Product Repositories

--- ProductAPI/Repositories/Abstract/IProductRepository.cs
+++ ProductAPI/Repositories/Abstract/IProductRepository.cs
@@ -4,7 +4,7 @@ namespace ProductAPI.Repositories.Abstract
 {
     public interface IProductRepository
     {
-        bool AddUpdate(Product Product);
+        bool AddUpdate(Product product);
         bool Delete(int id);
         Product GetById(int id);
         IEnumerable<Product> GetAll(string term = "");
---
+++ ProductAPI/Repositories/Implementation/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using ProductAPI.Data;
+using ProductAPI.Models.Domain;
+using ProductAPI.Repositories.Abstract;
+
+namespace ProductAPI.Repositories.Implementation
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool AddUpdate(Product product)
+        {
+            try
+            {
+                if (product.Id == 0)
+                {
+                    _context.Product.Add(product);
+                }
+                else
+                {
+                    _context.Product.Update(product);
+                }                  
+                _context.SaveChanges();
+                return true;
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var record = GetById(id);
+                if (record == null)
+                {
+                    return false;
+                }
+                _context.Product.Remove(record);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Product> GetAll(string term = "")
+        {
+            term = term.ToLower();
+            var data = (from product in _context.Product
+                        join
+                        category in _context.Category
+                        on product.CategoryId equals category.Id
+                        where term == "" || product.Name.ToLower().StartsWith(term)
+                        select new Product
+                        {
+                            Id = product.Id,
+                            CategoryId = product.CategoryId,
+                            Name = product.Name,
+                            CategoryName = category.Name,
+                            Price = product.Price
+                        }
+                        ).ToList();
+            return data;
+        }
+
+
+        public Product GetById(int id)
+        {
+            return _context.Product.Find(id);
+        }
+    }
+}
